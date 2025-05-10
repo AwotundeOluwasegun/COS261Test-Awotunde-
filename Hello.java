ad35f1c37a2ea072a14734a1844e2396fa83442a
Awotunde Quoyum Oluwasegun <102822936+AwotundeOluwasegun@users.noreply.github.com>
2025-05-10 11:08:08 +0100

    first commit
    hello world display

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package hello;
+public class Hello {
+
+    public static void main(String[] args) {
+        String intro = "Hello world!";  //Declaring and assigning a var_name intro and storing a value in it.
+        System.out.print(intro); //used to print out the word
+    }
+    
+}
